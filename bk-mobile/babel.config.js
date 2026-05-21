f1680bf3ef5ea163b5ac976576beab392235a3bf
thienba <huynhthienba4@gmail.com>
2026-05-21 09:48:42 +0700

    fix(mobile): fix babel config and add missing native deps (reanimated, webview)

--- bk-mobile/babel.config.js
+++ bk-mobile/babel.config.js
@@ -1,26 +1,8 @@
 module.exports = function (api) {
-  const isTest = api.env('test');
   api.cache(true);
-
-  const baseConfig = {
+  return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
     ],
-    plugins: [
-      'nativewind/babel',
-    ],
   };
-
-  if (isTest) {
-    // For tests, use babel-preset-expo but without nativewind
-    return {
-      presets: [
-        ['babel-preset-expo'],
-        ['@babel/preset-typescript', { allExtensions: true, isTSX: true }],
-      ],
-      plugins: [],
-    };
-  }
-
-  return baseConfig;
 };
